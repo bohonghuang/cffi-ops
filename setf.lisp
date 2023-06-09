29cfbffaf665c47cfa7af16a86c5affd3c1f0bef
coco24 <1281299809@qq.com>
2023-06-09 12:49:41 +0800

    Eliminate style warnings produced by `(setf (-> * * *) *)`

--- setf.lisp
+++ setf.lisp
@@ -8,9 +8,9 @@
 
 (define-setf-expander -> (init &rest exps &environment env)
   (multiple-value-bind (vars vals newval setter getter) (get-setf-expansion (expand-form `(-> ,init . ,exps)) env)
-    (declare (ignore newval setter))
+    (declare (ignore vars vals newval setter))
     (with-gensyms (store)
-      (values vars vals `(,store) `(setf ,getter ,store) getter))))
+      (values nil nil `(,store) `(setf ,getter ,store) getter))))
 
 (define-setf-expander [] (pointer &optional (index 0) &environment env)
   (multiple-value-bind (vars vals newval setter getter) (get-setf-expansion (expand-form `([] ,pointer ,index)) env)
