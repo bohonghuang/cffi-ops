90314c1516e1a5e94a6fc54a289ad6ba6465451c
coco24 <1281299809@qq.com>
2023-10-04 15:07:15 +0800

    Add package nickname `cops`

--- package.lisp
+++ package.lisp
@@ -1,6 +1,7 @@
 (defpackage cffi-ops
   (:use #:cl #:alexandria #:arrow-macros #:cffi #:trivial-macroexpand-all)
   (:import-from #:cffi #:ctype)
-  (:export #:ctype #:cthe #:clocally #:clet #:clet* #:csetf #:& #:-> #:[] #:foreign-alloca))
+  (:export #:ctype #:cthe #:clocally #:clet #:clet* #:csetf #:& #:-> #:[] #:foreign-alloca)
+  (:nicknames #:cops))
 
 (in-package #:cffi-ops)
